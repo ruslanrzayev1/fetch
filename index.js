e0d4186f33cced2aa385e0d37ffb7f9a51ddd85e
ruslanrzayev1 <sd7ga5a2n@code.edu.az>
2023-11-04 13:52:28 +0400

    fetch

---
+++ index.js
@@ -0,0 +1,101 @@
+// let inpFirst = document.getElementById("inpFirst");
+// let inpSecond = document.getElementById("inpSecond");
+// let btn = document.getElementById("btn");
+
+// function postCustomer() {
+//     axios.post('https://northwind.vercel.app/api/customers',{
+//         name: inpFirst.value,
+//         description: inpSecond.value
+//     })
+//     .then(res => {
+//         console.log(res);
+//     })
+// }
+// btn.addEventListener('click',postCustomer)
+
+
+
+
+// let btn = document.getElementById("btn");
+
+// function deleteCustomer() {
+//   axios.delete(`https://northwind.vercel.app/api/customers/${id = "AROUT"}`);
+// }
+// btn.addEventListener("click", deleteCustomer);
+
+
+
+
+
+// let btn = document.getElementById("btn");
+
+// function updateCustomer() {
+//     axios.put(`https://northwind.vercel.app/api/customers/${id = "BERGS"}`,id = "Ruslan")
+// }
+// btn.addEventListener("click", updateCustomer);
+
+// let inpFirst = document.getElementById("inpFirst");
+// let inpSecond = document.getElementById("inpSecond");
+// let btn = document.getElementById("btn");
+
+
+
+
+
+
+// function postFetch() {
+//   fetch("https://northwind.vercel.app/api/customers", {
+//     method: "post",
+//     headers: {
+//       "Content-Type": "application/json",
+//     },
+//     body: JSON.stringify({
+//       name: inpFirst.value,
+//       description: inpSecond.value,
+//     })
+//       .then((res) => res.json())
+//       .then((data) => {
+//         console.log(data);
+//       }),
+//   });
+// }
+// btn.addEventListener("click", postFetch);
+
+
+
+
+
+// let btn = document.getElementById("btn");
+
+// function deleteFetch() {
+//     fetch(`https://northwind.vercel.app/api/customers/${id = "BONAP"}`,{
+//         method: "delete"
+//     })
+// }
+// btn.addEventListener('click',deleteFetch)
+
+
+
+
+
+
+// let inpFirst = document.getElementById("inpFirst");
+// let inpSecond = document.getElementById("inpSecond");
+// let btn = document.getElementById('btn')
+
+// function putFetch() {
+//     fetch(`https://northwind.vercel.app/api/customers/${id = "BOLId"}`,{
+//         method: "PUT",
+//         headers: {
+//             "Content-Type": "application/json",
+//         },
+//         body: JSON.stringify({
+//            id: "Ruslan"
+//         })
+//         .then(res => res.json())
+//         .then(data => {
+//             console.log(data);
+//         }),
+//     });
+// }
+// btn.addEventListener('click',putFetch)
